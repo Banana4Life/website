d4bd9c72686b4c2729890ab126030d05ea098d9c
Jonas Dann <jonas.chr.dann@gmail.com>
2014-10-20 01:21:48 +0200

    Did some work on the website and restructured the project a bit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,13 +5,18 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.test())
+    Ok(views.html.main("Banana4Life - Blog")(views.html.blog()))
   }
 
-  def snippets = TODO
+  def snippets = Action {
+    Ok(views.html.main("Banana4Life - Snippets")(views.html.blog()))
+  }
 
-  def projects = TODO
+  def projects = Action {
+    Ok(views.html.main("Banana4Life - Projects")(views.html.blog()))
+  }
 
-  def about = TODO
-
+  def about = Action {
+    Ok(views.html.main("Banana4Life - About")(views.html.blog()))
+  }
 }
