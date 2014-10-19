e012452defd628069123301bbf734eec0af3bda7
Phillip Schichtel <phillip.public@schich.tel>
2014-10-19 20:05:16 +0200

    added a Global object

---
+++ app/Global.scala
@@ -0,0 +1,8 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+
+object Global extends GlobalSettings {
+  override def onHandlerNotFound(request: RequestHeader) = Future.successful(NotFound("OH NO! -> " + request.uri))
+}
