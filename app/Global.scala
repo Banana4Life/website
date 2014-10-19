df2896c338732790eef6c3e2af8ce53a5f6d68ac
Phillip Schichtel <phillip.public@schich.tel>
2014-10-19 22:06:28 +0200

    added a simple whitespace filter

--- app/Global.scala
+++ app/Global.scala
@@ -3,6 +3,6 @@ import play.api.mvc._
 import play.api.mvc.Results._
 import scala.concurrent.Future
 
-object Global extends GlobalSettings {
+object Global extends WithFilters(WhitespaceFilter) {
   override def onHandlerNotFound(request: RequestHeader) = Future.successful(NotFound("OH NO! -> " + request.uri))
 }
