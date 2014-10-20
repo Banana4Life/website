10a35fff3ad5bfd79d813b2329d3115e30fa37ac
Phillip Schichtel <phillip.public@schich.tel>
2014-10-20 12:24:04 +0200

    that's how you feed content to your templates

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,18 +5,18 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.main("Banana4Life - Blog")(views.html.blog()))
+    Ok(views.html.blog())
   }
 
   def snippets = Action {
-    Ok(views.html.main("Banana4Life - Snippets")(views.html.blog()))
+    Ok(views.html.blog())
   }
 
   def projects = Action {
-    Ok(views.html.main("Banana4Life - Projects")(views.html.blog()))
+    Ok(views.html.blog())
   }
 
   def about = Action {
-    Ok(views.html.main("Banana4Life - About")(views.html.blog()))
+    Ok(views.html.blog())
   }
 }
