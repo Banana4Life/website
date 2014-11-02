afc2eff98ecda38f8f969041c8304c6de739a2ac
Jonas Dann <jonas.chr.dann@gmail.com>
2014-11-02 02:14:28 +0100

    I got all names to work :D yay

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,7 +12,7 @@ object Application extends Controller {
 
     implicit object ListOfProjectsFormat extends Format[List[Project]] {
         override def writes(projects: List[Project]): JsValue = JsArray(projects.map( project => JsObject(List("name" -> JsString(project.name)))))
-        override def reads(json: JsValue): JsResult[List[Project]] = JsSuccess(List(Project((json \\ "name")(0).validate[String].get)))
+        override def reads(json: JsValue): JsResult[List[Project]] = JsSuccess(json.validate[List[JsValue]].get.map( project => Project((project \ "name").validate[String].get) ))
     }
 
     def index = Action {
