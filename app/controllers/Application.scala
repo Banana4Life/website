228c3295e2b04c22e1353c97907f6ce7f0ccec96
Jonas Dann <jonas.chr.dann@gmail.com>
2014-11-02 02:06:16 +0100

    First try using the github api

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,22 +1,39 @@
 package controllers
 
+import play.api.libs.json._
 import play.api.mvc._
+import play.api.Play.current
+import play.api.libs.ws._
+
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.blog())
-  }
+    implicit object ListOfProjectsFormat extends Format[List[Project]] {
+        override def writes(projects: List[Project]): JsValue = JsArray(projects.map( project => JsObject(List("name" -> JsString(project.name)))))
+        override def reads(json: JsValue): JsResult[List[Project]] = JsSuccess(List(Project((json \\ "name")(0).validate[String].get)))
+    }
 
-  def snippets = Action {
-    Ok(views.html.blog())
-  }
+    def index = Action {
+        Ok(views.html.blog())
+    }
 
-  def projects = Action {
-    Ok(views.html.blog())
-  }
+    def snippets = Action {
+        Ok(views.html.blog())
+    }
 
-  def about = Action {
-    Ok(views.html.blog())
-  }
+    def projects = Action.async {
+        val futureResponse = WS.url("https://api.github.com/orgs/Banana4Life/repos").withRequestTimeout(10000).get()
+        val futureProjects = futureResponse.map( response => Json.parse(response.body).as[List[Project]] )
+        futureProjects.map( projects => Ok(views.html.projects(projects)) )
+    }
+
+    def about = Action {
+        Ok(views.html.blog())
+    }
+}
+
+case class Project(name: String) {
+
 }
