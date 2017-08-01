8c8e626673f49b0ced6705155e3d5eb0ec2917aa
Anselm Brehme <development.anselm@breh.me>
2017-08-01 17:55:12 +0200

    load game on button click

--- public/javascripts/LoadWebVersion.js
+++ public/javascripts/LoadWebVersion.js
@@ -1,5 +1,11 @@
-var gameInstance = UnityLoader.instantiate("gameContainer", $("#gameContainer").data("buildjson"), {onProgress: UnityProgress});
-
-$(".fullscreen").on("click", function () {
-    gameInstance.SetFullscreen(1);
+var gameInstance;
+$("#load-game-button").on("click", function () {
+    $(this).addClass("started");
+    gameInstance = UnityLoader.instantiate("gameContainer", $("#gameContainer").data("buildjson"), {onProgress: UnityProgress});
+    var fs = $("#fullscreen-game-button");
+    fs.addClass("started");
+    fs.on("click", function () {
+        gameInstance.SetFullscreen(1);
+    });
 });
+
