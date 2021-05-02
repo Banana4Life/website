62f446783c4861d9e6fbcc23343cab1ab5a595e5
Phillip Schichtel <phillip@schich.tel>
2021-05-03 00:15:44 +0200

    add a generic node and add support for node2 APIs

--- app/service/Formats.scala
+++ app/service/Formats.scala
@@ -6,8 +6,6 @@ import play.api.libs.json._
 
 object Formats {
 
-    implicit val ldjamMeta: Format[LdjamMeta] = Json.format
-
     implicit object UrlFormat extends Format[URL] {
         override def reads(json: JsValue): JsResult[URL] = json match {
             case JsString(s) => JsSuccess(new URL(s))
@@ -15,19 +13,6 @@ object Formats {
         }
 
         override def writes(o: URL): JsValue = JsString(o.toString)
-    }
-
-    implicit object MetaFormat extends Format[Either[LdjamMeta, Int]] {
-        override def writes(o: Either[LdjamMeta, Int]): JsValue = o match {
-            case Left(meta) => ldjamMeta.writes(meta)
-            case _ => JsArray()
-        }
-
-        override def reads(json: JsValue): JsResult[Either[LdjamMeta, Int]] = json match {
-            case JsArray(_) => JsSuccess(Right(1))
-            case o: JsObject => ldjamMeta.reads(o).map(Left(_))
-            case _ => JsError("LdjamMeta must be an empty array or an object!")
-        }
     }
 
     implicit val repo: Format[Repo] = Json.format
--- app/service/LdjamService.scala
+++ app/service/LdjamService.scala
@@ -47,10 +47,10 @@ class LdjamService(conf: Configuration, cache: AsyncCacheApi, implicit val ec: E
             .withHttpHeaders("User-Agent" -> "Banana4Life")
             .withRequestTimeout(5.seconds)
 
-    private def get[T: Reads](url: String): Future[T] =
+    private def get[T <: ApiResponse: Reads](url: String): Future[T] =
         request(url).get().flatMap { response =>
           if (response.status == 200) Future.successful(response.json.as[T])
-          else Future.failed(new LdJamApiException(response.status, response.body))
+          else Future.failed(new LdJamApiException(url, response.status, response.body))
         }
 
     private def findPostNodeIdsForUser(userid: Int): Future[Seq[Int]] = {
@@ -193,6 +193,11 @@ class LdjamService(conf: Configuration, cache: AsyncCacheApi, implicit val ec: E
         get[NodeGetResponse](url)
     }
 
+    def getNodes2(nodes: Seq[Int]): Future[NodeGetResponse] = {
+        val url = s"$apiBaseUrl/vx/node2/get/${urlList(nodes)}"
+        get[NodeGetResponse](url)
+    }
+
     def walk(root: Int, path: String): Future[NodeWalkResponse] = {
         if (!path.startsWith("/")) {
             throw new IllegalArgumentException("path must begin with a /")
@@ -201,9 +206,16 @@ class LdjamService(conf: Configuration, cache: AsyncCacheApi, implicit val ec: E
         val url = s"$apiBaseUrl/vx/node/walk/$root$path"
         get[NodeWalkResponse](url)
     }
-}
 
-case class LdjamMeta(avatar: Option[String])
+    def walk2(root: Int, path: String): Future[Node2WalkResponse] = {
+        if (!path.startsWith("/")) {
+            throw new IllegalArgumentException("path must begin with a /")
+        }
+
+        val url = s"$apiBaseUrl/vx/node2/walk/$root$path"
+        get[Node2WalkResponse](url)
+    }
+}
 
 case class LdjamPost(id: Int, name: String, author: UserNode, body: String, createdAt: ZonedDateTime, authorLink: String, avatarLink: Option[String]) extends BlogPost {
     override def anchor: String = s"ldjam_$id"
@@ -215,12 +227,24 @@ case class LdJamEntry(id: Int, name: String, body: String)
 
 case class LdjamEvent(id: Int, name: String, body: String)
 
-class LdJamApiException(status: Int, body: String) extends RuntimeException
-case class NodeGetResponse(status: Int, caller_id: Int, cached: Seq[Int], node: Seq[Node])
+class LdJamApiException(url: String, status: Int, body: String) extends RuntimeException(s"$url -> $status -> $body")
+
+sealed trait ApiResponse {
+    val status: Int
+    val caller_id: Int
+}
+
+final case class NodeGetResponse(status: Int, caller_id: Int, cached: Seq[Int], node: Seq[Node]) extends ApiResponse
 object NodeGetResponse {
     implicit val format: Format[NodeGetResponse] = Json.format
 }
-case class NodeFeedResponse(status: Int, caller_id: Int, method: Seq[String], types: Seq[String], offset: Int, limit: Int, feed: Seq[NodeFeedEntry])
+
+final case class Node2GetResponse(status: Int, caller_id: Int, cached: Seq[Int], node: Seq[Node]) extends ApiResponse
+object Node2GetResponse {
+    implicit val format: Format[Node2GetResponse] = Json.format
+}
+
+final case class NodeFeedResponse(status: Int, caller_id: Int, method: Seq[String], types: Seq[String], offset: Int, limit: Int, feed: Seq[NodeFeedEntry]) extends ApiResponse
 object NodeFeedResponse {
     implicit val format: Format[NodeFeedResponse] = Json.format
 }
@@ -228,9 +252,14 @@ case class NodeFeedEntry(id: Int, modified: Instant)
 object NodeFeedEntry {
     implicit val format: Format[NodeFeedEntry] = Json.format
 }
-case class NodeWalkResponse(status: Int, caller_id: Int, root: Int, path: Seq[Int], node: Int)
+final case class NodeWalkResponse(status: Int, caller_id: Int, root: Int, path: Seq[Int], node: Int) extends ApiResponse
 object NodeWalkResponse {
     implicit val format: Format[NodeWalkResponse] = Json.format
+}
+
+final case class Node2WalkResponse(status: Int, caller_id: Int, root: Int, path: Seq[Int], node_id: Int, nodes_cached: Seq[Int], node: Seq[Node]) extends ApiResponse
+object Node2WalkResponse {
+    implicit val format: Format[Node2WalkResponse] = Json.format
 }
 
 sealed trait Node {
@@ -258,15 +287,16 @@ object Node {
             case e: EventNode => implicitly[Writes[EventNode]].writes(e)
             case a: UserNode => implicitly[Writes[UserNode]].writes(a)
             case p: PostNode => implicitly[Writes[PostNode]].writes(p)
-            case p: GameNode => implicitly[Writes[GameNode]].writes(p)
+            case g: GameNode => implicitly[Writes[GameNode]].writes(g)
+            case x: GenericNode => implicitly[Writes[GenericNode]].writes(x)
         }
 
         override def reads(json: JsValue): JsResult[Node] = (json \ "type", json \ "subtype", json \ "subsubtype") match {
             case (JsDefined(JsString("event")), _, _) => EventNode.format.reads(json)
             case (JsDefined(JsString("user")), _, _) => UserNode.format.reads(json)
             case (JsDefined(JsString("post")), _, _) => PostNode.format.reads(json)
-            case (JsDefined(JsString("item")), JsDefined(JsString("game")), _) => PostNode.format.reads(json)
-            case _ => JsError("unknown node type")
+            case (JsDefined(JsString("item")), JsDefined(JsString("game")), _) => GameNode.format.reads(json)
+            case _ => GenericNode.format.reads(json)
         }
     }
 }
@@ -312,7 +342,10 @@ case object FuzzyNone extends FuzzyOption[Nothing] {
     override def toOption: Option[Nothing] = None
 }
 
-
+final case class GenericNode(id: Int, parent: Int, superparent: Int, author: Int, `type`: String, subtype: FuzzyOption[String], subsubtype: FuzzyOption[String], published: Instant, created: Instant, modified: Instant, version: Int, slug: String, name: String, body: String, path: String, parents: Seq[Int], meta: FuzzyOption[Map[String, String]], love: Int) extends Node
+object GenericNode {
+    implicit val format: Format[GenericNode] = Json.format
+}
 
 final case class UserNode(id: Int, parent: Int, superparent: Int, author: Int, `type`: String, subtype: FuzzyOption[String], subsubtype: FuzzyOption[String], published: Instant, created: Instant, modified: Instant, version: Int, slug: String, name: String, body: String, path: String, parents: Seq[Int], love: Int, meta: FuzzyOption[UserMetadata], games: Int, articles: Int, posts: Int) extends Node
 case class UserMetadata(avatar: Option[String])
@@ -333,7 +366,17 @@ object PostNode {
     implicit val format: Format[PostNode] = Json.format
 }
 
-final case class GameNode(id: Int, parent: Int, superparent: Int, author: Int, `type`: String, subtype: FuzzyOption[String], subsubtype: FuzzyOption[String], published: Instant, created: Instant, modified: Instant, version: Int, slug: String, name: String, body: String, path: String, parents: Seq[Int], love: Int) extends Node
+final case class GameNode(id: Int, parent: Int, superparent: Int, author: Int, `type`: String, subtype: FuzzyOption[String], subsubtype: FuzzyOption[String], published: Instant, created: Instant, modified: Instant, version: Int, slug: String, name: String, body: String, path: String, parents: Seq[Int], love: Int, meta: GameMetadata, grades: Option[Map[String, Int]], notes: Int, `notes-timestamp`: Option[Instant], magic: GameMagic) extends Node
 object GameNode {
     implicit val format: Format[GameNode] = Json.format
+}
+
+case class GameMetadata(author: Seq[Int], `allow-anonymous-comments`: Option[JsValue], cover: Option[String], `link-01`: Option[String], `link-01-tag`: Option[JsValue])
+object GameMetadata {
+    implicit val format: Format[GameMetadata] = Json.format
+}
+
+case class GameMagic(cool: Double, feedback: Int, given: Double, grade: Double, smart: Double)
+object GameMagic {
+    implicit val format: Format[GameMagic] = Json.format
 }
