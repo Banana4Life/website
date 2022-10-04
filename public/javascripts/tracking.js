decbde51a231da134beda7479041e8c7a5708d99
Phillip Schichtel <phillip@schich.tel>
2022-10-04 15:14:56 +0200

    drop piwik

--- public/javascripts/tracking.js
+++
@@ -1,5 +0,0 @@
-var _paq = _paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-_paq.push(['setTrackerUrl', '//analytics.cubyte.org/piwik.php']);
-_paq.push(['setSiteId', 6]);
