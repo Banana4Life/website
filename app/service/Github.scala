c4bd4458a75995ecac5cdb0592c8a604bb575f86
Anselm Brehme <development.anselm@breh.me>
2025-10-03 18:37:54 +0200

    refactor GithubService + disable authorlist when no core member found

--- app/service/Github.scala
+++ app/service/Github.scala
@@ -72,6 +72,8 @@ case class Team(name: String, id: Int, slug: String, description: String) derive
 case class Member(login: String, id: Int) derives ConfiguredDecoder
 case class User(login: String, name: String, core: Boolean = false)
 
+case class Members(core: Seq[User], guests: Seq[User])
+
 class GithubService(ws: WSClient, cache: SyncCacheApi, config: Configuration, implicit val ec: ExecutionContext) {
 
     private val logger = Logger(classOf[GithubService])
@@ -81,141 +83,159 @@ class GithubService(ws: WSClient, cache: SyncCacheApi, config: Configuration, im
     private val apiOrgBase = apiBase + "/orgs/"
     private val reposUrl = s"$apiOrgBase$orga/repos"
     private val teamsUrl = s"$apiOrgBase$orga/teams"
+    private val graphQlUrl = "https://api.github.com/graphql"
+    private val projectJsonFile = ".banana4life/project.json"
     private def teamsMembersUrl(teamId: Int) = s"$apiBase/teams/$teamId/members"
     private def teamsReposUrl(teamId: Int) = s"$apiBase/teams/$teamId/repos"
     private val CoreTeam = "Core"
 
-
     implicit val localDateOrdering: Ordering[ZonedDateTime] = Ordering.by(_.toEpochSecond)
-
-    def getProjects: Future[Seq[Project]] = {
-        cache.getOrElseUpdate(ProjectsCacheKey, CacheDuration) {
-            for {
-                responseRepos <- ws.url(reposUrl).withRequestTimeout(10000.milliseconds).get()
-                teams <- getTeams
-                projects <- complete(responseRepos.body[List[Repo]], teams)
-            } yield {
-                projects
-            }
-        }
-    }
 
     def getCurrent: Future[Option[Project]] = {
         getProjects.map(l => l.headOption.flatMap {
             p => if (p.createdAt.isAfter(ZonedDateTime.now().minus(24, ChronoUnit.DAYS))) Some(p) else None
         })
-
     }
 
+    def getProjects: Future[Seq[Project]] = {
+        cache.getOrElseUpdate(ProjectsCacheKey, CacheDuration) {
+            for {
+                repos <- getRepos
+                repoMembersMap <- getRepoMembersMap
+                projects <- Future.sequence(repos.map(repo => getProjectMeta(repo).map((repo, _))))
+            } yield {
+                projects.filter((_, meta) => meta != null)
+                        .map((repo, meta) => {
+                              val members = repoMembersMap.getOrElse(repo.name, Members(Seq.empty, Seq.empty))
+                              fullProject(repo, members, meta)
+                        }).sortBy(_.createdAt)(using localDateOrdering.reverse)
+            }
+        }
+    }
 
-    def complete(projectBasics: Seq[Repo], teams: Seq[(Team, (Seq[User], Seq[User]), Seq[Repo])]): Future[Seq[Project]] = {
+    private def getRepos = {
+        ws.url(reposUrl).withRequestTimeout(10000.milliseconds).get().map(_.body[List[Repo]])
+    }
 
-        val memberLookup = (for {
-            (_, u, r) <- teams
-            repo <- r
-        } yield {
-            (repo.name, u)
-        }).toMap
-        val futures = projectBasics map { basics =>
-            val fileName = ".banana4life/project.json"
-            given derivation.Configuration = derivation.Configuration.default.withDefaults
-            ws.url(basics.file(fileName)).get().map { response =>
-                response.status match {
-                    case 200 =>
-                        val meta = response.body[ProjectMeta]
-                        val date = meta.date
-                          .map(d => d.atStartOfDay(ZoneId.systemDefault()))
-                          .getOrElse(basics.created_at)
-                        val (core, guests) = memberLookup.getOrElse(basics.name, (Seq.empty, Seq.empty))
-                        Project(basics.name, meta.name, URI(basics.html_url), meta.description, meta.jam,
-                            meta.authors.sorted, URI(basics.file(".banana4life/main.png")), date,
-                            meta.download.getOrElse(basics.latestRelease), meta.soundtrack, meta.web, meta.cheats.getOrElse(Nil),
-                            core.sortBy(_.name), guests.sortBy(_.name), meta.videos.getOrElse(Seq.empty))
-                    case 404 =>
-                        logger.info(s"Missing $fileName for project ${basics.full_name}!")
-                        null
-                    case _ =>
-                        logger.warn(s"Failed to request $fileName for project ${basics.full_name}!")
-                        null
-                }
-            }.recover({
-                case _: ParsingFailure =>
-                    logger.warn(s"Failed to parse $fileName for project ${basics.full_name}!")
+    private def fullProject(repo: Repo, members: Members, meta: ProjectMeta): Project = {
+        val date = meta.date
+          .map(d => d.atStartOfDay(ZoneId.systemDefault()))
+          .getOrElse(repo.created_at)
+
+        Project(repo.name,
+                meta.name,
+                URI(repo.html_url),
+                meta.description,
+                meta.jam,
+                meta.authors.sorted,
+                URI(repo.file(".banana4life/main.png")),
+                date,
+                meta.download.getOrElse(repo.latestRelease),
+                meta.soundtrack,
+                meta.web,
+                meta.cheats.getOrElse(Nil),
+                members.core.sortBy(_.name),
+                members.guests.sortBy(_.name),
+                meta.videos.getOrElse(Seq.empty))
+    }
+
+    private def getProjectMeta(repo: Repo) = {
+        ws.url(repo.file(projectJsonFile)).get().map { response =>
+            response.status match {
+                case 200 =>
+                    response.body[ProjectMeta]
+                case 404 =>
+                    logger.info(s"Missing $projectJsonFile for project ${repo.full_name}!")
                     null
-                case e: Exception =>
-                    logger.error(s"Failed to complete project info for ${basics.full_name}!", e)
+                case _ =>
+                    logger.warn(s"Failed to request $projectJsonFile for project ${repo.full_name}!")
                     null
-            })
-        }
-
-        Future.sequence(futures) map { projects =>
-            projects.filter(_ != null).sortBy(_.createdAt).reverse
-        }
-    }
-
-    def getWebVersion() = {
-        ws.url("https://banana4life.github.io/LegendarySpaceSpaceSpace/latest/").get()
-    }
-
-    def getTeams = {
-
-        def withAuth(url: String) = {
-            ws.url(url).withAuth(config.get[String]("github.tokenUser"), config.get[String]("github.token"), BASIC).get()
-        }
-
-        for {
-            response <- withAuth(teamsUrl)
-            teams = if ((200 to 299) contains response.status) response.body[List[Team]] else Seq()
-            members <- Future.sequence(teams.map(team => withAuth(teamsMembersUrl(team.id)).map(_.body[List[Member]])))
-            repos <- Future.sequence(teams.map(team => withAuth(teamsReposUrl(team.id)).map(_.body[List[Repo]])))
-            allUsers <- getMembers
-        } yield {
-            val allTeams = teams zip members zip repos map {
-                case ((t, m), r) => (t, m, r)
             }
-            val coreTeam = allTeams.find(_._1.name == CoreTeam).map(_._2).getOrElse(Set.empty[Member]).map(_.login).toSet
+        }.recover({
+            case _: ParsingFailure =>
+                logger.warn(s"Failed to parse $projectJsonFile for project ${repo.full_name}!")
+                null
+            case e: Exception =>
+                logger.error(s"Failed to complete project info for ${repo.full_name}!", e)
+                null
+        })
+    }
 
-            def getUser(m: Seq[Member]) = {
-                m.map { mm =>
-                    val name = allUsers.get(mm.login).map(_.name).getOrElse(mm.login)
-                    User(mm.login, name, coreTeam.contains(mm.login))
+    private def withBasicAuth(url: String) = {
+        ws.url(url).withAuth(config.get[String]("github.tokenUser"), config.get[String]("github.token"), BASIC)
+    }
+
+    private def getRepoMembersMap = {
+        if (!config.has("github.tokenUser")) {
+            logger.warn("No github token! Cannot fetch team member information!")
+            Future.successful(Map.empty[String, Members])
+        } else {
+            for {
+                teams <- getTeams
+                members <- Future.sequence(teams.map(team => getTeamMembers(team)))
+                repos <- Future.sequence(teams.map(team => getTeamRepos(team)))
+                allUsers <- getAllMembers
+            } yield {
+                val allTeams = teams zip members zip repos map {
+                    case ((t, m), r) => (t, m, r)
                 }
-            }
+                val coreTeam = allTeams.find(_._1.name == CoreTeam).map(_._2).getOrElse(Set.empty[Member]).map(_.login).toSet
 
-            allTeams.map {
-                case (t, m, r) => (t, getUser(m).partition(_.core), r)
+                def getUser(m: Seq[Member]) = {
+                    m.map { mm =>
+                        val name = allUsers.get(mm.login).map(_.name).getOrElse(mm.login)
+                        User(mm.login, name, coreTeam.contains(mm.login))
+                    }
+                }
+
+                (for {
+                    (team, members, repos) <- allTeams
+                    users = getUser(members).partition(_.core)
+                    repo <- repos
+                } yield {
+                    (repo.name, Members(users._1, users._2))
+                }).toMap
             }
         }
     }
 
+    private def getTeams = {
+        withBasicAuth(teamsUrl).get().map(response => {
+            if ((200 to 299) contains response.status) response.body[List[Team]] else Seq()
+        })
+    }
 
-    def getMembers = {
+    private def getTeamRepos(team: Team) = {
+        withBasicAuth(teamsReposUrl(team.id)).get().map(_.body[List[Repo]])
+    }
+
+    private def getTeamMembers(team: Team) = {
+        withBasicAuth(teamsMembersUrl(team.id)).get().map(_.body[List[Member]])
+    }
+
+    private def getAllMembers = {
         implicit val bodyWritableOfJson: BodyWritable[Json] = BodyWritable(json => InMemoryBody(ByteString.fromString(json.noSpaces)), "application/json")
         val query = Json.obj("query" -> Json.fromString("query { organization(login:\"Banana4Life\") { membersWithRole(last:100) { nodes { login, name } } } }"))
-        ws.url("https://api.github.com/graphql").withAuth(config.get[String]("github.tokenUser"), config.get[String]("github.token"), BASIC)
-          .post(query)
+        withBasicAuth(graphQlUrl).post(query)
           .map(_.json)
-          .map { v =>
-                logger.info(v.toString())
-                v \ "data" \ "organization" \ "membersWithRole" \ "nodes"
-            } collect {
-                case JsDefined(JsArray(nodes)) => nodes.map { value =>
-                    val login = (value \ "login").get
-                    val name = value \ "name" match {
-                        case JsDefined(n) =>
-                            n match {
-                                case JsNull => login
-                                case JsString("") => login
-                                case _ => n
-                            }
-                        case _ => login
-                    }
+          .map { v => v \ "data" \ "organization" \ "membersWithRole" \ "nodes" } collect {
+            case JsDefined(JsArray(nodes)) => nodes.map { value =>
+                val login = (value \ "login").get
+                val name = value \ "name" match {
+                    case JsDefined(n) =>
+                        n match {
+                            case JsNull => login
+                            case JsString("") => login
+                            case _ => n
+                        }
+                    case _ => login
+                }
 
-                    User(login.as[String], name.as[String])
-                  }
-                case _ => Seq.empty
-            } map { users =>
-                users.map(user => (user.login, user)).toMap
+                User(login.as[String], name.as[String])
             }
+            case _ => Seq.empty
+        } map { users =>
+            users.map(user => (user.login, user)).toMap
+        }
     }
 }
